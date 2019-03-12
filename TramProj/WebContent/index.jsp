a1ac8c0f28ef8ba7856f5b1566966111fed184ac
kosmo_24 <kosmo_24@DESKTOP-C8L90HH>
2019-03-12 12:06:39 +0900

    학원에서 두번째 파일 생성 및 수정

--- TramProj/WebContent/index.jsp
+++ TramProj/WebContent/index.jsp
@@ -9,7 +9,13 @@
 <body>
 	<ul>
 		<li>학원에서 만든 index파일</li>
-		<li>첫번째 수정111</li>
+		<li>파일 수정:KOSMO</li>
+		<li>
+			<%
+				DTO dto =new DTO("홍길동");
+				out.println(dto.getName());
+			%>
+		</li>
 	</ul>
 </body>
 </html>
---
+++ TramProj/src/DTO.java
@@ -0,0 +1,23 @@
+
+public class DTO {
+ String name;
+ 
+
+public DTO() {
+	super();
+}
+
+public DTO(String name) {
+	super();
+	this.name = name;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+ 
+}
